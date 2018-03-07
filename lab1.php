488ff11d913cdf1ad61da8feb66dcc516c3951e2
yiwang88 <yiwang88@hotmail.com>
2018-03-07 16:32:23 -0600

    Create lab1.php

---
+++ lab1.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!extension_loaded('ldap')) {
+
+    echo "Need to load PHP ldap extension </ br>";
+    echo "</br> <a href='https://docs.microsoft.com/en-us/azure/app-service/web-sites-php-configure'>Get instruction here</a>";
+}
+else {
+    phpinfo();
+}
+?>
