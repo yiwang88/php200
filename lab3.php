a2be7ac6b2fcb0c9ec85035202b85903d409b7e2
yiwang88 <yiwang88@hotmail.com>
2018-03-07 16:14:24 -0600

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -1,12 +1,16 @@
 <?php
-if ( ini_get('realpath_cache_size') != 64) {
+if ( ini_get('realpath_cache_size') != 64K) {
     echo "Current realpath_cache_size = " . ini_get('realpath_cache_size') . "</ br>";
     
-    echo "Increase this to 64 KB" . "</ br>";
+    echo "</br > Increase this to 64 KB" . "</ br>";
 
     echo "</br> <a href='https://docs.microsoft.com/en-us/azure/app-service/web-sites-php-configure'>Get instruction here</a>";
 
-    echo "</br> <a href='http://php.net/manual/en/errorfunc.configuration.php'>Reference: PHP Runtime Configuration</a>";
+    echo "</br > References:
+    echo "</br> <a href='http://php.net/manual/en/ini.list.php'>List of php.ini directives</a>";
+    echo "</br> <a href='http://php.net/manual/en/configuration.changes.modes.php'>Where a configuration setting may be set</a>";
+    
+    
 }
 else {
     phpinfo();
