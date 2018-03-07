ab3863fdb3e6fc5d9ace9f26d0f398adcf72b28f
yiwang88 <yiwang88@hotmail.com>
2018-03-07 16:37:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-if (!extension_loaded('ldap')) {
-    echo "Need to load PHP ldap extension </ br>";
- 
-    echo "</br> <a href='https://docs.microsoft.com/en-us/azure/app-service/web-sites-php-configure'>Get instruction here</a>";
-}
-else {
-    phpinfo();
-}
+    echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as <your-site-name>/lab#.php" . "</br>";
+
+    echo "</br> <a href='lab1'>Lab 1</a>";
+    echo "</br> <a href='lab1'>Lab 1</a>";
+    echo "</br> <a href='lab1'>Lab 1</a>";
+    echo "</br> <a href='lab1'>Lab 1</a>";
 ?>
