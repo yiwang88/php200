a9f3674081b7dca178cc3c20e5cfb6ef8d791c25
yiwang88 <yiwang88@hotmail.com>
2018-03-07 15:36:00 -0600

    Update lab2.php

--- lab2.php
+++ lab2.php
@@ -1,9 +1,9 @@
 <?php
-if (!extension_loaded('zip')) {
-    echo "Need to install PHP zip extension </ br>";
+if (!extension_loaded('mongodb')) {
+    echo "Need to install PHP mongodb extension </ br>";
     
     echo "</br> <a href='https://docs.microsoft.com/en-us/azure/app-service/web-sites-php-configure'>Get instruction here</a>";
-    echo "</br> <a href='https://pecl.php.net/package/zip'>PECL Download site</a>";
+    echo "</br> <a href='https://pecl.php.net/package/mongodb'>PECL Download site</a>";
 }
 else {
     phpinfo();
