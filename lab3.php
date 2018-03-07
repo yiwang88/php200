ba51ebf8e4b78e8228b467c39e7c9860a9c9a67d
yiwang88 <yiwang88@hotmail.com>
2018-03-07 15:59:16 -0600

    Create lab3.php

---
+++ lab3.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!extension_loaded('mongodb')) {
+
+    echo "Current realpath_cache_size = " . ini_get('realpath_cache_size') . "</ br>";
+    
+    echo "Increase this to 64 KB" . "</ br>";
+
+    echo "</br> <a href='https://docs.microsoft.com/en-us/azure/app-service/web-sites-php-configure'>Get instruction here</a>";
+
+    echo "</br> <a href='http://php.net/manual/en/errorfunc.configuration.php'>Reference: PHP Runtime Configuration</a>";
+
+
+}
+
+else {
+
+    phpinfo();
+
+}
+
+?>
