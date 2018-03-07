03179d5c87c4ba27167c2f528b37158bc408eafc
yiwang88 <yiwang88@hotmail.com>
2018-03-07 17:05:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as <your-site-name>/lab#.php" . "</br>";
+    echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as Your-Site-Name/lab#.php" . "</br>";
 
     //echo "</br> <a href='lab1'>Lab 1</a>";
     //echo "</br> <a href='lab1'>Lab 1</a>";
