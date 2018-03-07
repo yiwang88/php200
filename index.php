d96481019084a535b716fc1bd10406127c8e39bb
yiwang88 <yiwang88@hotmail.com>
2018-03-07 17:01:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
-    //echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as <your-site-name>/lab#.php" . "</br>";
+    echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as <your-site-name>/lab#.php" . "</br>";
 
     //echo "</br> <a href='lab1'>Lab 1</a>";
     //echo "</br> <a href='lab1'>Lab 1</a>";
     //echo "</br> <a href='lab1'>Lab 1</a>";
     //echo "</br> <a href='lab1'>Lab 1</a>";
 
+echo "</br> <a href='/lab1.php'>Lab 1: Load PHP ldap extension</a>";
+echo "</br> <a href='/lab2.php'>Lab 2: Install PHP mongodb extension</a>";
+echo "</br> <a href='/lab3.php'>Lab 3: Modify the value of realpath_cache_size</a>";
+echo "</br> <a href='/lab4.php'>Lab 4: Remove zip extension (Setup custom PHP runtime)</a>";
 
-
+/*
 if (!extension_loaded('ldap')) {
     echo "Need to load PHP ldap extension </ br>";
 
@@ -20,5 +24,5 @@ else {
     phpinfo();
 
 }
-
+*/
 ?>
