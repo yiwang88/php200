495f5479bbd8d03ef4bb054ae72014f30825beed
yiwang88 <yiwang88@hotmail.com>
2018-03-12 15:41:52 -0500

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -4,11 +4,9 @@ if ( ini_get('realpath_cache_size') != 2048) {
     
     echo "</br > Change this to 2048 KB" . "</ br>";
 
-    echo "</br> <a href='https://blogs.msdn.microsoft.com/azureossds/2018/03/09/change-php_ini_system-configuration-settings'>Get instruction here</a>" . "</br >";
-
-    echo "</br > References: ";
-    echo "</br> <a href='http://php.net/manual/en/ini.list.php'>List of php.ini directives</a>";
-    echo "</br> <a href='http://php.net/manual/en/configuration.changes.modes.php'>Where a configuration setting may be set</a>";
+    //echo "</br> <a href='https://blogs.msdn.microsoft.com/azureossds/2018/03/09/change-php_ini_system-configuration-settings'>Get instruction here</a>" . "</br >";
+    //echo "</br> <a href='http://php.net/manual/en/ini.list.php'>List of php.ini directives</a>";
+    //echo "</br> <a href='http://php.net/manual/en/configuration.changes.modes.php'>Where a configuration setting may be set</a>";
     
     
 }
