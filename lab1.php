b1e17bbc0de29d892960db90cc0f2b0f476be6ff
yiwang88 <yiwang88@hotmail.com>
2018-03-12 14:14:33 -0500

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -6,9 +6,10 @@ if (!extension_loaded('ldap')) {
     //echo "</br> <a href='https://blogs.msdn.microsoft.com/azureossds/2017/08/21/enable-php-extensions-on-azure-web-app-on-windows/'>Get instruction here</a>";
 }
 else {
-    echo "LDAP extension has been installed, you are ready to go!";
-    //phpinfo();
-    $exts = get_loaded_extensions();
-    var_dump($exts);
+    echo "LDAP extension has been installed, you are ready to go! </ br>";
+
+    echo '</br > Installed extensions: </br >';
+
+    print_r(get_loaded_extensions());
 }
 ?>
