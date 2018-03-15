d98e9bf171a18b0c9ba9c56a57794af098f2d55b
yiwang88 <yiwang88@hotmail.com>
2018-03-14 19:23:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 echo "</br> <a href='/lab1.php'>Lab 1: LDAP APPLICATION ERROR #1404 LDAP Extension Not Loaded</a>";
 echo "</br> <a href='/lab2.php'>Lab 2: PHP Error: Class 'MongoDB\Client' not found</a>";
-echo "</br> <a href='/lab3.php'>Lab 3: Modify the value of realpath_cache_size</a>";
+echo "</br> <a href='/lab3.php'>Lab 3: Modify realpath_cache_size</a>";
 echo "</br> <a href='/lab4.php'>Lab 4: Remove zip extension (Setup custom PHP runtime)</a>";
 
 
