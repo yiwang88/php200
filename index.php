0658d5f983be788656964af8708990b4d05254c8
yiwang88 <yiwang88@hotmail.com>
2018-03-14 19:22:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as Your-Site-Name/lab#.php" . "</br>";
 
 echo "</br> <a href='/lab1.php'>Lab 1: LDAP APPLICATION ERROR #1404 LDAP Extension Not Loaded</a>";
-echo "</br> <a href='/lab2.php'>Lab 2: Install PHP mongodb extension</a>";
+echo "</br> <a href='/lab2.php'>Lab 2: PHP Error: Class 'MongoDB\Client' not found</a>";
 echo "</br> <a href='/lab3.php'>Lab 3: Modify the value of realpath_cache_size</a>";
 echo "</br> <a href='/lab4.php'>Lab 4: Remove zip extension (Setup custom PHP runtime)</a>";
 
