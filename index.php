d956b22bc58dc705988ff0a0c0c6d01ba0448569
yiwang88 <yiwang88@hotmail.com>
2018-03-14 19:21:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     echo "Welcome to PHP L200! You can use following links to access 4 labs, or access the lab as Your-Site-Name/lab#.php" . "</br>";
 
-echo "</br> <a href='/lab1.php'>Lab 1: Load PHP ldap extension</a>";
+echo "</br> <a href='/lab1.php'>Lab 1: LDAP APPLICATION ERROR #1404 LDAP Extension Not Loaded</a>";
 echo "</br> <a href='/lab2.php'>Lab 2: Install PHP mongodb extension</a>";
 echo "</br> <a href='/lab3.php'>Lab 3: Modify the value of realpath_cache_size</a>";
 echo "</br> <a href='/lab4.php'>Lab 4: Remove zip extension (Setup custom PHP runtime)</a>";
