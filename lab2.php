097d6af635b69c30119d9e7a00aad67e6367c856
yiwang88 <yiwang88@hotmail.com>
2018-03-21 16:09:52 -0500

    Update lab2.php

--- lab2.php
+++ lab2.php
@@ -1,9 +1,6 @@
 <?php
 if (!extension_loaded('mongodb')) {
     echo "PHP Error: Class 'MongoDB\Client' not found </ br>";
-    
-    //echo "</br> <a href='https://docs.microsoft.com/en-us/azure/app-service/web-sites-php-configure'>Get instruction here</a>";
-    //echo "</br> <a href='https://pecl.php.net/package/mongodb'>PECL Download site</a>";
 }
 else {
     echo "Mongodb extension has been installed, find it from installed extensions, you are ready to go! </ br>";
