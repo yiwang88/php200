50d3eb2fc898196d0f57457e4c273f6cb6b11987
yiwang88 <yiwang88@hotmail.com>
2018-03-21 16:12:02 -0500

    Update lab4.php

--- lab4.php
+++ lab4.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $allext = get_loaded_extensions(true);
@@ -6,13 +5,11 @@ if (!in_array("the ionCube PHP Loader", $allext)) {
 
     echo "My script requires ionCube loaders Do you have ionCube installed? </ br>";
     echo "</br> <a href='https://www.ioncube.com/loaders.php'>Download ionCube Loader Here</a>";
-    //echo "</br> <a href='https://github.com/projectkudu/kudu/wiki/Xdt-transform-samples#using-a-custom-phpini'>Get instruction here</a>";
+    
 }
-
 else {
     echo "Zend extension ionCube Loader is installed, ready to go! </br >";
     var_dump($allext);
 
 }
-
 ?>
