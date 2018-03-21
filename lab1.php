ddefd0884108969b27ab2ce37effb86393374992
yiwang88 <yiwang88@hotmail.com>
2018-03-21 16:09:24 -0500

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -3,7 +3,7 @@
 if (!extension_loaded('ldap')) {
 
     echo "LDAP APPLICATION ERROR #1404 LDAP Extension Not Loaded. </ br>";
-    //echo "</br> <a href='https://blogs.msdn.microsoft.com/azureossds/2017/08/21/enable-php-extensions-on-azure-web-app-on-windows/'>Get instruction here</a>";
+    
 }
 else {
     echo "LDAP extension has been installed, you are ready to go! </ br>";
