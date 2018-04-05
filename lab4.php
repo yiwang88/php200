be65cdfe20bad6f35c1a83cef25fcfee69f62a47
yiwang88 <yiwang88@hotmail.com>
2018-04-05 09:11:46 -0500

    Update lab4.php

--- lab4.php
+++ lab4.php
@@ -1,15 +1,4 @@
 <?php
+echo "Hello World!;
 
-$allext = get_loaded_extensions(true);
-if (!in_array("the ionCube PHP Loader", $allext)) {
-
-    echo "My script requires ionCube loaders Do you have ionCube installed? </ br>";
-    echo "</br> <a href='https://www.ioncube.com/loaders.php'>Download ionCube Loader Here</a>";
-    
-}
-else {
-    echo "Zend extension ionCube Loader is installed, ready to go! </br >";
-    var_dump($allext);
-
-}
 ?>
